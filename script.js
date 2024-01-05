d5cc3e54ad8696786b531302f695f2b68ac94fce
Poonam Mishra <poonammishra@Poonams-MacBook-Air.local>
2024-01-05 16:51:21 +0530

    pop-up

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("dddddd")
+document.querySelector('.popup-button').addEventListener('click',()=>{document.getElementById('popup').style.display='block'})
+
+function closePopUp(){
+    document.getElementById('popup').style.display='none'
+}
